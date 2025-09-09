9c85a56074b59ac40fa969a4378944aa478c4ba1
EmanuelCav <cavallinema@gmail.com>
2025-09-09 14:36:23 -0300

    backlog

--- utils/src/utils/conexiones/conexiones.h
+++ utils/src/utils/conexiones/conexiones.h
@@ -9,24 +9,26 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
+#define BACKLOG 10
+
 /**
  * @brief Inicia un servidor en la ip y puerto correspondientes
- * 
+ *
  * @param ip: ip del servidor que estamos creando
  * @param puerto: puerto del servidor que estamos creando
  * @return int: ID del file descriptor del socket del servidor
- * 
+ *
  * @example int socketServidor = iniciar_servidor(2340);
  */
 int iniciar_servidor(char *puerto);
 
 /**
  * @brief Inicia un cliente conectandose a un servidor en la ip y puerto correspondientes
- * 
+ *
  * @param ip: ip del servidor al que nos conectamos
  * @param puerto: puerto del servidor al cual nos conectamos
  * @return int: ID del file descriptor del socket creado con el servidor
- * 
+ *
  * @example int socketCliente = conectar_a_servidor("192.168.0.1", 2340);
  */
 int conectar_servidor(char *ip, char *puerto);
