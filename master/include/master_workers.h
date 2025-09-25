d0c1924544de3f22413d262674523a543b1b35e8
EmanuelCav <cavallinema@gmail.com>
2025-09-25 09:38:12 -0300

    Fuction master

--- master/include/master_workers.h
+++ master/include/master_workers.h
@@ -27,8 +27,9 @@ extern int cantidad_workers;
  *
  * @param socket Socket del worker recién conectado
  * @param logger Logger para imprimir el log obligatorio
+ * @param worker_id id del worker
  */
-void registrar_worker(int socket, t_log *logger);
+void registrar_worker(int socket, t_log *logger, int worker_id)
 
 /**
  * @brief Envía una query a un Worker disponible, busca en la cola de ready
