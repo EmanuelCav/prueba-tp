ed6d39136a004b6a85a965444ef1d5296ef70c61
EmanuelCav <cavallinema@gmail.com>
2025-10-08 17:07:04 -0300

    imports

--- master/include/master_workers.h
+++ master/include/master_workers.h
@@ -1,13 +1,25 @@
 #ifndef MASTER_WORKERS_H
 #define MASTER_WORKERS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
 #include <pthread.h>
 #include <commons/log.h>
+
 #include "master_queues.h"
 
 #define MAX_WORKERS 50
 
-typedef struct {
+typedef struct
+{
     int socket;
     int worker_id;
     bool ocupado;
