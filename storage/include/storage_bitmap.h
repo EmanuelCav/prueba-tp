d24d50d33e11b969ba87282d9530bbf8d0a67279
EmanuelCav <cavallinema@gmail.com>
2025-10-29 15:11:47 -0300

    import errorh

--- storage/include/storage_bitmap.h
+++ storage/include/storage_bitmap.h
@@ -6,11 +6,26 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <errno.h>
 #include <commons/log.h>
 
 #include "storage_config.h"
 
+/**
+ * @brief Busca el primer bloque libre disponible en el bitmap y lo marca como ocupado.
+ *
+ * @param cfg Puntero a la estructura de configuración del storage (t_storage_config),
+ * @param logger Puntero al logger utilizado para registrar mensajes e información de errores.
+ */
 int buscar_bloque_libre(t_storage_config *cfg, t_log *logger);
+
+/**
+ * @brief Libera un bloque físico previamente ocupado en el bitmap.
+ *
+ * @param bloque Número del bloque físico a liberar.
+ * @param cfg Puntero a la estructura de configuración del storage (t_storage_config),
+ * @param logger Puntero al logger utilizado para registrar mensajes e información de errores.
+ */
 void liberar_bloque(int bloque, t_storage_config *cfg, t_log *logger);
 
 #endif
